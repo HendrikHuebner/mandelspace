868648debc225bbb2b76e029dd089e4e3ee31a4b
hhuebner <ge84pip@mytum.de>
2024-08-21 09:07:38 +0700

    Added opengl test file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "util/Config.hpp"
 #include <cstdlib>
 #include <iostream>
+#include "core/mandelbrot.hpp"
 
 int main() {
     Config cfg("/home/hhuebner/Documents/mandelspace/settings.cfg");
 
     std::cout << cfg.test3  << std::endl;
-
+    render_mandelbrot();
     return EXIT_SUCCESS;
 }
